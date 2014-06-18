856fdc6dc153d13c9b523f5459276b3eb6fd2f22
acbart <acbart@vt.edu>
2014-06-18 09:49:49 -0400

    Latest updates stuff

--- controllers/tools.py
+++ controllers/tools.py
@@ -9,7 +9,11 @@ def status():
         total_log += log.read()
     return PRE(total_log)
     
-def build_chapters():    
+def build_chapters():
+    def unCamel(x):
+        return reduce(lambda a,b: a + ((b.upper() == b and
+                      (len(a) and a[-1].upper() != a[-1])) and
+                      (' ' + b) or b), x, '')
     # Clear out the old chapters
     current_compthink_chapters = db(db.chapters.course_id == 2)
     current_compthink_chapters.delete()
@@ -21,35 +25,33 @@ def build_chapters():
     #regex for parsing html is evil, but proper parsing would take longer and
     # be as imprecise, not to mention more complicated!
     import re
-    output = P()
+    output = P("Deleted existing chapter information")
+    output += P("Adding Chapter Info to DB")
     links = re.findall('(?:<h\d>(.*)<a.*href="#(.*?)")' # Match up the chapter names
                        '|' # Or match up the section names
                        '(?:<li class="toctree-.*"reference internal" href="(.*?)">(.*?)</a></li>)',
                        index_page)
-    # Auto-generated code gives us some assumptions:
+    # Auto-generated html gives us some assumptions:
     #   * Always starts with a chapter, and the sections follow its chapters, so
     #     we can assume the previous chapter_name was the chapter
     for chapter_name, chapter_label, sub_url, sub_name in links:
         if chapter_name and chapter_label:
             last_chapter_name, last_chapter_label = chapter_name, chapter_label
-            output += P(chapter_name + ":" + chapter_label)
+            current_row_id = db.chapters.insert(chapter_name=chapter_name,
+                                              course_id=2,
+                                              chapter_label=chapter_label)
+            output += P("Found Chapter \"{}\" ({})".format(chapter_name, chapter_label))
         else:
             if '/' in sub_url:
                 sub_label = sub_url.split('/')[1].split('.')[0]
             else:
                 sub_label = sub_url.split('.')[0]
-            output += P(" ::: ".join((last_chapter_name, last_chapter_label, sub_name, sub_label)))
+            db.sub_chapters.insert(sub_chapter_name=unCamel(sub_name),
+                                   chapter_id=current_row_id,
+                                   sub_chapter_label=sub_label)
+            output += P("Found subchapter \"{}\" ({})".format(sub_name, sub_label))
+    db.commit()
     return output
-    
-    #result = "Adding Chapter Info to DB"
-    #for chapter in subChapD:
-    #    print chapter
-    #    currentRowId = db.chapters.insert(chapter_name=chapTitles[chapter],course_id=course_id,chapter_label=chapter)
-    #    for subchaptername in subChapD[chapter]:
-    #        db.sub_chapters.insert(sub_chapter_name=unCamel(subchaptername),
-    #                               chapter_id=currentRowId,
-    #                               sub_chapter_label=subchaptername)
-    #    db.commit()
 
 def rebuild():
     with open("book_build.log", "w") as log:
@@ -66,5 +68,6 @@ def rebuild():
             call(["/users/acbart/build_prod.sh"], stdout=log)
         else:
             log.write("Given: " + repr(data) + "\n")
-        log.write("Completed.")
+        log.write("Completed. Building chapters.")
+        log.write(build_chapters().flatten())
         return ref
